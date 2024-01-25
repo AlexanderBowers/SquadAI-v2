c155d0be98fc9b756fa10cc06e9a0522036e75cd
AlexanderBowers <7319abowers@gmail.com>
2024-01-24 17:23:45 -0800

    Rebuilt binaries. Now compiling with previous AIController functions.
    Next step is to turn them into best practices.

--- Source/Squadv2/SquadAIController.h
+++ Source/Squadv2/SquadAIController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "SquadPlayerController.h"
 #include "TimerManager.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "SquadAIController.generated.h"
@@ -17,15 +18,64 @@ class SQUADV2_API ASquadAIController : public AAIController
 	GENERATED_BODY()
 
 public:
+	FCommandPoint LastCommand;
+	TArray<FCommandPoint> LocalCommandList; //In case I need to store multiple commands
 
 
+	UPROPERTY(EditAnywhere)
+	FCommandPoint PriorityCommand;
+	UPROPERTY(EditAnywhere)
+	bool bShouldFollow = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Indoors")
+	bool bIsIndoors = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bHasPriority = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AActor* AssignedPosition;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Indoors")
+	AActor* Building;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Indoors")
+	AActor* Room;
+
+	UFUNCTION()
+	void ClearRoom();
+	UFUNCTION()
+	void MoveToCommand(FCommandPoint CommandPoint);
+
+	UFUNCTION(BlueprintCallable)
+	void ResetPriorityCommand();
+
+	UFUNCTION(BlueprintCallable)
+	void OnUpdated(AActor* NewActor);
 
 
 
 protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	class UAIPerceptionComponent* AIPerceptionComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	class UAISenseConfig_Sight* SightConfig;
+
+
+
+
+	UFUNCTION()
+	void HandleCommand(FCommandPoint CommandPoint);
+	UFUNCTION()
+	void FollowPlayer();
 
 
 private:
+	UPROPERTY()
+	ASquadPlayerController* PlayerController;
+	UPROPERTY()
+	FTimerHandle TimerHandle;
+
+	void SetupPerceptionSystem();
 
 
 };
